810ab9cdacece7f3a46f0991030f00053e4b0ca8
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-06-17 19:17:56 +0200

    dockerizing the application

---
+++ ror/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+while ! nc -z mysql 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
+echo "MySQL Server is ready"
+
+while ! nc -z elasticsearch 9200 ; do
+    echo "Waiting for the ElasticSearch Server"
+    sleep 3
+done
+echo "ElasticSearch Server is ready"
+
+echo "Starting Migrating ..."
+rails db:migrate
+echo "Migrating is finished"
+
+echo "Starting Rails Server ..."
+rails server -b 0.0.0.0
